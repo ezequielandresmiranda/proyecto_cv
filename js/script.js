233f2203a39b72f8266c096f4c11e71adf8da939
Eze Miranda <126541239+ezequielandresmiranda@users.noreply.github.com>
2023-03-01 15:43:25 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// moviento de barra lataral
+
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
+
